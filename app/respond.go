ae64216d71d3fb600f2243c55ffd71e24eacfe51
Alfan Nur Fauzan <alpancs@gmail.com>
2018-12-24 20:28:30 +0700

    update print format

--- app/respond.go
+++ app/respond.go
@@ -14,7 +14,7 @@ var apiURL = fmt.Sprintf("https://api.telegram.org/bot%s/", os.Getenv("BOT_TOKEN
 func respondUpdate(update *telegram.Update) error {
 	_, err := http.PostForm(apiURL+"sendMessage", url.Values{
 		"chat_id": {fmt.Sprintf("%d", update.Message.Chat.ID)},
-		"text":    {fmt.Sprintf("%+v", update)},
+		"text":    {fmt.Sprintf("%#v", update)},
 	})
 	return err
 }
