1aef1e8364dac7fcd75b1b136ebaf3ad51e25443
Alfan Nur Fauzan <alpancs@gmail.com>
2018-12-24 14:16:48 +0700

    echo bot

--- app/respond.go
+++ app/respond.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
 
 	telegram "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -10,5 +12,9 @@ import (
 var apiURL = fmt.Sprintf("https://api.telegram.org/bot%s/", os.Getenv("BOT_TOKEN"))
 
 func respondUpdate(update *telegram.Update) error {
-	return nil
+	_, err := http.PostForm(apiURL+"sendMessage", url.Values{
+		"chat_id": {fmt.Sprintf("%d", update.Message.Chat.ID)},
+		"text":    {fmt.Sprintf("you wrote %s", update.Message.Text)},
+	})
+	return err
 }
