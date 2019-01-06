4b9d4a6e7ef4d20068542bea7aa2b61c2a2be233
Alfan Nur Fauzan <alpancs@gmail.com>
2019-01-06 21:59:46 +0700

    try to implement /catat command handler

--- app/respond.go
+++ app/respond.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -11,32 +10,36 @@ import (
 	telegram "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-var apiURL = fmt.Sprintf("https://api.telegram.org/bot%s/", os.Getenv("BOT_TOKEN"))
+var (
+	apiURL         = fmt.Sprintf("https://api.telegram.org/bot%s/", os.Getenv("BOT_TOKEN"))
+	sendMessageURL = apiURL + "sendMessage"
+)
 
 func respondUpdate(update *telegram.Update) error {
-	replyText, err := json.MarshalIndent(update, "", " ")
-	if err != nil {
-		return err
-	}
-
-	if update.Message == nil {
-		update.Message = update.EditedMessage
-	}
 	if update.Message == nil {
 		return nil
 	}
 
-	resp, err := http.PostForm(apiURL+"sendMessage", url.Values{
-		"chat_id":    {fmt.Sprintf("%d", update.Message.Chat.ID)},
-		"text":       {"```\n" + string(replyText) + "\n```"},
-		"parse_mode": {"Markdown"},
-	})
-
-	if resp.StatusCode >= 300 {
-		fmt.Println("response code:", resp.StatusCode)
-		fmt.Println("response body:")
-		io.Copy(os.Stdout, resp.Body)
+	if right, err := commandCatat(update.Message); right {
+		return err
 	}
 
-	return err
+	return nil
+}
+
+func commandCatat(msg *telegram.Message) (bool, error) {
+	if msg.Command() == "catat" {
+		resp, err := http.PostForm(sendMessageURL, url.Values{
+			"chat_id":      {fmt.Sprintf("%d", msg.Chat.ID)},
+			"text":         {"apa saja yang ingin dicatat?"},
+			"reply_markup": {"force_reply"},
+		})
+		if err == nil && resp.StatusCode >= 300 {
+			fmt.Println("response code:", resp.StatusCode)
+			fmt.Println("response body:")
+			io.Copy(os.Stdout, resp.Body)
+		}
+		return true, err
+	}
+	return false, nil
 }
