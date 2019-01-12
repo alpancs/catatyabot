3d375f81c1e1024a7f22c34ed9d646908aae5e2a
Alfan Nur Fauzan <alpancs@gmail.com>
2019-01-12 21:33:05 +0700

    refactor testing

--- app/price_test.go
+++ app/price_test.go
@@ -2,30 +2,24 @@ package main
 
 import "testing"
 
-func TestPriceString0(t *testing.T) {
-	p := Price(0)
-	if p.String() != "0" {
-		t.Error("expected 0, got", p)
+func assertEqual(t *testing.T, expected, actual string) {
+	if expected != actual {
+		t.Errorf("expected %s, actual %s", expected, actual)
 	}
+}
+
+func TestPriceString0(t *testing.T) {
+	assertEqual(t, "0", Price(0).String())
 }
 
 func TestPriceString12(t *testing.T) {
-	p := Price(12)
-	if p.String() != "12" {
-		t.Error("expected 12, got", p)
-	}
+	assertEqual(t, "12", Price(12).String())
 }
 
 func TestPriceString2500(t *testing.T) {
-	p := Price(2500)
-	if p.String() != "2.500" {
-		t.Error("expected 2500, got", p)
-	}
+	assertEqual(t, "2.500", Price(2500).String())
 }
 
 func TestPriceString1234567(t *testing.T) {
-	p := Price(1234567)
-	if p.String() != "1.234.567" {
-		t.Error("expected 1234567, got", p)
-	}
+	assertEqual(t, "1.234.567", Price(1234567).String())
 }
