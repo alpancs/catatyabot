a27a6e283961672952febe6808e656c5c3420982
Alfan Nur Fauzan <alpancs@gmail.com>
2019-01-22 21:01:58 +0700

    update readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# @catatyabot
+
+*@catatyabot* (https://t.me/catatyabot) adalah bot Telegram untuk membantu mencatat pengeluaran sehari-hari.
+
+# Cara Kerja
+
+Cara @catatyabot membantu anda:
+- undang @catatyabot ke grup Telegram keluarga anda
+- `/catat` untuk memanggil bot supaya bersiap mencatat 📝
+- `/hapus` untuk menghapus catatan 🗑️
+- `/lihat` untuk melihat catatan 👀
+- `/rangkum` untuk melihat rangkuman catatan 💰
+
+Selain itu, anda juga dapat mengubah catatan dengan cara membalas/_reply_ ke pesan `#catatan` yang ingin diubah, lalu sebutkan nama barang serta harga barang yang baru.
+
+# Contoh Catatan
+- _sayur kangkung 2 ribu_
+- _lombok 1/2 kg 3,5k_
+- _motor CB150R 27jt_
--- app/start.go
+++ app/start.go
@@ -4,15 +4,14 @@ import (
 	telegram "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-const StartMessage = `cara Asisten Belanja membantu anda:
+const StartMessage = `Cara @catatyabot membantu anda:
 - undang @catatyabot ke grup Telegram keluarga anda
 - /catat untuk memanggil bot supaya bersiap mencatat 📝
 - /hapus untuk menghapus catatan 🗑️
 - /lihat untuk melihat catatan 👀
 - /rangkum untuk melihat rangkuman catatan 💰
 
-selain itu, anda juga dapat mengubah catatan dengan cara membalas/_reply_ ke pesan #catatan yang ingin diubah, lalu sebutkan nama barang serta harga barang yang baru
-`
+Selain itu, anda juga dapat mengubah catatan dengan cara membalas/_reply_ ke pesan #catatan yang ingin diubah, lalu sebutkan nama barang serta harga barang yang baru.`
 
 func commandStart(msg *telegram.Message) (bool, error) {
 	if msg.Command() != "start" {
