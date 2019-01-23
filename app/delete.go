f30208b8b9aee4264e6633929c6630d71d8d1e27
Alfan Nur Fauzan <alpancs@gmail.com>
2019-01-23 08:25:46 +0700

    remove #catatan after deleting

--- app/delete.go
+++ app/delete.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"strings"
+
 	telegram "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
@@ -25,6 +27,7 @@ func commandDelete(msg *telegram.Message) (bool, error) {
 	if rowsAffected == 0 {
 		_, err = sendMessage(msg.Chat.ID, "hapus apa to bos? 🙄", msg.MessageID)
 	} else {
+		editMessage(msg.Chat.ID, msg.ReplyToMessage.MessageID, strings.Replace(msg.ReplyToMessage.Text, " #catatan", "", -1))
 		_, err = sendMessage(msg.Chat.ID, "ini sudah dihapus ya bos 🚮", msg.ReplyToMessage.MessageID)
 	}
 	return true, err
