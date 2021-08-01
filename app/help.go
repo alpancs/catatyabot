3041a5dbf3c672ccca7fd49e257a9c4608e5dcd9
Alfan Nur Fauzan <alpancs@gmail.com>
2021-08-01 21:33:56 +0700

    minor

--- app/help.go
+++ app/help.go
@@ -5,6 +5,7 @@ import (
 )
 
 const StartMessage = `*== cara @catatyabot membantu anda ==*
+
 1. undang @catatyabot ke grup Telegram anda, atau boleh juga langsung _chat_ ke bot 😊
 2. panggil @catatyabot menggunakan perintah berikut,
    - /catat membuat bot bersiap mencatat 📝, lalu balas dengan menuliskan satu atau beberapa catatan
