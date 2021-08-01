4e39ce975398059f7322f039807163b8a020c39f
Alfan Nur Fauzan <alpancs@gmail.com>
2021-08-01 21:40:38 +0700

    spaces

--- app/help.go
+++ app/help.go
@@ -7,10 +7,15 @@ import (
 const StartMessage = `*== cara @catatyabot membantu anda ==*
 
 1. undang @catatyabot ke grup Telegram anda, atau boleh juga langsung _chat_ ke bot 😊
+
 2. panggil bot menggunakan perintah berikut,
+
     - /catat membuat bot bersiap mencatat 📝, lalu balas dengan menuliskan satu atau beberapa catatan sekaligus
+
     - _reply_ pesan #catatan dari bot dengan /hapus untuk menghapusnya 🗑️
+
     - gunakan /lihat untuk melihat daftar catatan 👀
+
     - dan gunakan /rangkum untuk merangkum catatan selama beberapa waktu terakhir 📈📉
 
 satu pesan boleh berisi lebih dari satu catatan lho 🙂
