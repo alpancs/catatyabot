6e244893734f271edf3ad9580f23a5a521345821
Alfan Nur Fauzan <alpancs@gmail.com>
2021-12-06 05:56:55 +0700

    handle separators on negative values

--- app/price.go
+++ app/price.go
@@ -31,7 +31,7 @@ func (p Price) String() string {
 	return separate(strconv.FormatInt(int64(p), 10))
 }
 func separate(s string) string {
-	if len(s) <= 3 {
+	if len(strings.TrimPrefix(s, "-")) <= 3 {
 		return s
 	}
 	return separate(s[:len(s)-3]) + "." + s[len(s)-3:]
