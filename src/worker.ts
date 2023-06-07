197313553e24d27ea32a7eadbc3c2e0b2f647675
Alfan Nur Fauzan <alpancs@gmail.com>
2023-06-07 23:29:27 +0700

    refactor

--- src/worker.ts
+++ src/worker.ts
@@ -7,18 +7,18 @@ export default {
 	async fetch(request: Request, env: Env) {
 		const { pathname } = new URL(request.url);
 		if (request.method == "POST" && pathname == "/webhook/telegram") {
-			return handleRequestWebhookTelegram(request, env);
+			return this.handleRequestWebhookTelegram(request, env);
 		}
 		return new Response(null, { status: 404 });
 	},
-};
 
-async function handleRequestWebhookTelegram(request: Request, env: Env) {
-	if (!request.headers.has("X-Telegram-Bot-Api-Secret-Token")) {
-		return new Response(null, { status: 401 });
-	}
-	if (request.headers.get("X-Telegram-Bot-Api-Secret-Token") != env.TELEGRAM_TOKEN) {
-		return new Response(null, { status: 403 });
-	}
-	return Response.json({ message: "OK" });
-}
+	async handleRequestWebhookTelegram(request: Request, env: Env) {
+		if (!request.headers.has("X-Telegram-Bot-Api-Secret-Token")) {
+			return new Response(null, { status: 401 });
+		}
+		if (request.headers.get("X-Telegram-Bot-Api-Secret-Token") != env.TELEGRAM_TOKEN) {
+			return new Response(null, { status: 403 });
+		}
+		return Response.json({ message: "OK" });
+	},
+};
