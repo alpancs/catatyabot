bbef5bc32e4961dcb79325aaa898d7d619366ca8
Alfan Nur Fauzan <alpancs@gmail.com>
2023-06-14 01:06:40 +0700

    try to do a proper bug fix

--- src/delete.ts
+++ src/delete.ts
@@ -1,21 +1,19 @@
+import { thousandSeparated } from "./read";
 import { escapeUserInput } from "./send";
-import { answerPattern as itemPattern } from "./create";
 
 export const noItemToDelete = "mau hapus pesan yang mana? balas pesan bot 👆 yang berisi catatan pakai perintah /hapus";
 
 export async function replyForItemDeletion(send: SendTextFn, edit: EditTextFn, chatId: number, replyToMessageId: number, replyToMessageText: string, db: D1Database) {
     try {
-        await db.prepare("DELETE FROM items WHERE chat_id = ?1 AND message_id = ?2")
-            .bind(chatId, replyToMessageId).run();
+        const deletedItem = await db.prepare("DELETE FROM items WHERE chat_id = ?1 AND message_id = ?2 RETURNING *")
+            .bind(chatId, replyToMessageId).first<Item | null>();
+        if (deletedItem) {
+            await edit(replyToMessageId, `~${escapeUserInput(deletedItem.name)} ${thousandSeparated(deletedItem.price)}~`);
+            return send(`${escapeUserInput(deletedItem.name)} ${thousandSeparated(deletedItem.price)} sudah dihapus 🚮`);
+        }
+        return send(`${replyToMessageText} tidak ada di catatan 🙄`);
     } catch (error: any) {
         console.error({ message: error.message, cause: error.cause.message });
         return send(`"${escapeUserInput(replyToMessageText)}" ga bisa dihapus 😵`);
     }
-
-    try {
-        await edit(replyToMessageId, `~${escapeUserInput(replyToMessageText)}~`);
-    } catch (error: any) {
-        console.error({ replyToMessageId, replyToMessageText, error });
-    }
-    return send(`${escapeUserInput(replyToMessageText)} sudah dihapus 🚮`);
 }
