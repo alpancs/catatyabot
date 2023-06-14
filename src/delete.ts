cc579329ef9623ccdac1f03917fe5ed792f91534
Alfan Nur Fauzan <alpancs@gmail.com>
2023-06-14 18:39:05 +0700

    refine delete interaction

--- src/delete.ts
+++ src/delete.ts
@@ -9,12 +9,11 @@ export async function replyForItemDeletion(send: SendTextFn, edit: EditTextFn, c
         const deletedItem = await db.prepare("DELETE FROM items WHERE chat_id = ?1 AND message_id = ?2 RETURNING *")
             .bind(chatId, replyToMessageId).first<Item | null>();
         if (deletedItem) {
-            await edit(replyToMessageId, `~${escapeUserInput(deletedItem.name)} ${thousandSeparated(deletedItem.price)}~`);
-            return send(`${escapeUserInput(deletedItem.name)} ${thousandSeparated(deletedItem.price)} sudah dihapus 🚮`);
+            await edit(replyToMessageId, `~${escapeUserInput(deletedItem.name)} ${thousandSeparated(deletedItem.price)} dicatat~`);
+            await send(`${escapeUserInput(deletedItem.name)} ${thousandSeparated(deletedItem.price)} sudah dihapus 🚮`);
         }
-        return send(`yang mau dihapus tidak ada di catatan 🙄`);
     } catch (error: any) {
         console.error({ message: error.message, cause: error.cause.message });
-        return send(`ada masalah pas lagi hapus catatan 😵`);
+        await send(`ada masalah pas lagi hapus catatan 😵`);
     }
 }
