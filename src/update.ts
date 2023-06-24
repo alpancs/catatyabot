8ecdb0627b1fcda0cd05a4c085571a6740b3e148
Alfan Nur Fauzan <alpancs@gmail.com>
2023-06-24 10:40:07 +0700

    simplify update process

--- src/update.ts
+++ src/update.ts
@@ -4,16 +4,12 @@ import { escapeUserInput } from "./send";
 
 export async function replyForItemUpdate(send: SendTextFn, edit: EditTextFn, chatId: number, replyToMessage: Message, itemMatch: RegExpMatchArray, db: D1Database) {
     try {
-        const existingItem = await db.prepare("SELECT * FROM items WHERE chat_id = ?1 AND message_id = ?2")
-            .bind(chatId, replyToMessage.message_id).first<Item | null>();
-        if (existingItem) {
-            const { name, price } = parseItemMatch(itemMatch);
-            await db.prepare("UPDATE items SET name = ?3, price = ?4 WHERE chat_id = ?1 AND message_id = ?2")
-                .bind(chatId, replyToMessage.message_id, name, price).run();
-            await edit(replyToMessage.message_id, `~${replyToMessage.text}~\n*${escapeUserInput(name)}* *${thousandSeparated(price)}*`);
-        }
+        const { name, price } = parseItemMatch(itemMatch);
+        const updatedItem = await db.prepare("UPDATE items SET name = ?3, price = ?4 WHERE chat_id = ?1 AND message_id = ?2 RETURNING *")
+            .bind(chatId, replyToMessage.message_id, name, price).first<Item | null>();
+        if (updatedItem) return edit(replyToMessage.message_id, `~${replyToMessage.text}~\n*${escapeUserInput(name)}* *${thousandSeparated(price)}*`);
     } catch (error: any) {
         console.error({ message: error.message, cause: error.cause.message });
-        await send(`ada masalah pas lagi ubah catatan 😵`);
+        return send(`ada masalah pas lagi ubah catatan 😵`);
     }
 }
