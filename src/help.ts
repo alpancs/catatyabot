39e288cf4a10de61a008410b98dd85a3d8c21d21
Alfan Nur Fauzan <alpancs@gmail.com>
2023-06-26 23:56:12 +0700

    update help message

--- src/help.ts
+++ src/help.ts
@@ -4,17 +4,31 @@ export const helpMessage = `*=== CARA MENGGUNAKAN @catatyabot ===*
 
 2. Panggil bot menggunakan perintah berikut
 
-  - /catat untuk membuat bot bersiap mencatat, lalu balas dengan menuliskan apa yang mau dicatat berikut dengan harganya.
-    Satu pesan boleh berisi lebih dari satu catatan lho (1 baris untuk 1 catatan), dan bisa pakai satuan ribu/rb/k/juta/jt juga 🙂, contohnya seperti ini 👇
+  - /catat untuk membuat bot bersiap mencatat, lalu balas dengan menuliskan apa yang mau dicatat, berikut dengan harganya. Satu pesan bisa diisi lebih dari satu catatan lho (1 baris untuk 1 catatan), bisa pakai satuan ribu/rb/k/juta/jt juga 🙂, dan bisa pakai #hashtag juga 🎉.
+
+    Contohnya seperti ini 👇
 
     _sayur kangkung 2500_
-    _ayam 1 kg 27k_
-    _susu 86 ribu_
-    _sapu serba guna 1.75jt_
+    _tas #sekolah 135rb_
+    _#jajan kitkat 60k_
+    _sapu serba guna 4.4jt #perabot_
 
-  - /lihat untuk melihat daftar catatan.
-    Contohnya: _3 hari_, _2 pekan_, _1 bulan_, _dari awal_, _semua_, atau _semuanya_.
+
+  - /lihat untuk melihat daftar catatan. Contoh
+    - _3 hari_
+    - _2 pekan_
+    - _1 bulan_
+    - _dari awal_
+    - _semua_
+    - _semuanya_
+
+    Melihat daftar catatan yang punya #hashtag tertentu
+    - _#jajan 1 bulan_
+    - _semua #jajan_
+    - _#sekolah 1 tahun terakhir_
+
 
   - /hapus untuk menghapus suatu catatan, dengan cara terlebih dahulu membalas (_reply_) catatan dari bot (yang ada tanda ✅nya) yang ingin dihapus.
 
-  Selain itu anda juga dapat mengubah suatu catatan. Caranya dengan membalas catatan dari bot (yang ada tanda ✅nya) dengan nama & harga barang yang baru.`;
+
+Selain itu anda juga dapat mengubah suatu catatan. Caranya dengan membalas catatan dari bot (yang ada tanda ✅nya) dengan nama, #hashtag, dan harga barang yang baru.`;
