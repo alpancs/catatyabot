a91e45abf497f45b706be9c17bbac088a90a2ee8
Alfan Nur Fauzan <alpancs@gmail.com>
2023-06-29 13:58:13 +0700

    refactor

--- src/telegram.ts
+++ src/telegram.ts
@@ -17,11 +17,13 @@ async function respondMessage(message: Message, env: Env) {
     const send = (text: string) => sendMessage(env.TELEGRAM_BOT_TOKEN, message.chat.id, text);
     const ask = (text: string) => sendMessage(env.TELEGRAM_BOT_TOKEN, message.chat.id, text, true);
     const edit = (messageId: number, text: string) => editMessage(env.TELEGRAM_BOT_TOKEN, message.chat.id, messageId, text);
+
     if (message.text?.match(/^\s*\/?(start|bantuan)(@catatyabot)?\s*$/i)) return send(helpMessage);
     if (message.text?.match(/^\s*\/?catat(@catatyabot)?\s*$/i)) return ask(createItemsQuestion);
     if (message.text?.match(/^\s*\/?lihat(@catatyabot)?\s*$/i)) return ask(readItemsQuestion);
     if (message.text?.match(/^\s*\/?hapus(@catatyabot)?\s*$/i)) return message.reply_to_message ?
         replyForItemDeletion(send, edit, message.chat.id, message.reply_to_message, env.DB) : send(noItemToDelete);
+
     if (message.reply_to_message?.text === createItemsQuestion && message.text)
         return replyForItemsCreation(send, edit, message.text, env.DB);
     if (message.reply_to_message?.text === readItemsQuestion && message.text)
@@ -29,10 +31,9 @@ async function respondMessage(message: Message, env: Env) {
     const itemMatch = message.text?.match(itemPattern);
     if (message.reply_to_message && itemMatch)
         return replyForItemUpdate(send, edit, message.chat.id, message.reply_to_message, itemMatch, env.DB);
+
     console.info({ status: "ignored", reason: "the message does not match any cases", message });
-    ignoredMessageCounts[message.chat.id] = (ignoredMessageCounts[message.chat.id] ?? 0) + 1;
-    if (ignoredMessageCounts[message.chat.id] > 3) {
-        ignoredMessageCounts[message.chat.id] = 0;
-        return send("kalau bingung bisa pencet /bantuan atau tanya langsung ke @alpancs 💁‍♂️");
-    }
+    ignoredMessageCounts[message.chat.id] = ((ignoredMessageCounts[message.chat.id] ?? 0) + 1) % 3;
+    if (ignoredMessageCounts[message.chat.id] === 0)
+        await send("kalau bingung bisa pencet /bantuan atau tanya langsung ke @alpancs 💁‍♂️");
 }
