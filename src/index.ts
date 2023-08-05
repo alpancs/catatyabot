dae353bb59a27f8f45bd0db2af938636f24967df
Alfan Nur Fauzan <alpancs@gmail.com>
2023-08-05 12:41:39 +0700

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,18 @@ import { getUpdateResponse } from "./telegram/index";
 
 export default {
 	async fetch(request: Request, env: Env) {
-		const { pathname } = new URL(request.url);
-		if (request.method == "POST" && pathname == "/webhook/telegram") {
+		if (request.method === "POST" && new URL(request.url).pathname === "/webhook/telegram")
 			return this.handleWebhookTelegram(request, env);
-		}
 		return new Response(undefined, { status: 404 });
 	},
 
 	async handleWebhookTelegram(request: Request, env: Env) {
-		if (request.headers.get("X-Telegram-Bot-Api-Secret-Token") !== env.TELEGRAM_WEBHOOK_SECRET_TOKEN) {
+		if (request.headers.get("X-Telegram-Bot-Api-Secret-Token") !== env.TELEGRAM_WEBHOOK_SECRET_TOKEN)
 			return new Response(undefined, { status: 401 });
-		}
-		let update: Update;
 		try {
-			update = await request.json();
+			return getUpdateResponse(await request.json(), env);
 		} catch (error: any) {
 			return new Response(error, { status: 422 })
 		}
-		return getUpdateResponse(update, env);
 	},
 };
